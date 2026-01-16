4b609a8084913dea3756e58b709cb4a0f29093d2
Liabilitically <tarakesh.rameshkumar@gmail.com>
2026-01-15 23:54:27 -0600

    getting rid of public secrets

--- app/utilities/firebase.ts
+++ app/utilities/firebase.ts
@@ -3,12 +3,12 @@ import { addDoc, arrayRemove, arrayUnion, collection, doc, getDoc, getDocs, getF
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqMKBRhfWRQA-P1JVvq5d-mNaVArEtdRs",
-  authDomain: "surf-d0af1.firebaseapp.com",
-  projectId: "surf-d0af1",
-  storageBucket: "surf-d0af1.firebasestorage.app",
-  messagingSenderId: "967446961488",
-  appId: "1:967446961488:web:b738d60b7baabd437ca055"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
